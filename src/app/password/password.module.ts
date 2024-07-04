d219ab3244974cca8387ce7a3a46da0a13c9162e
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-07-04 12:32:29 -0600

    responsive design added

--- src/app/password/password.module.ts
+++ src/app/password/password.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+
 import { PasswordComponent } from './password/password.component';
 
 @NgModule({
